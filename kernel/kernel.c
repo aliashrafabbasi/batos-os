8f5fac1690dad589518d91ad8d47094611c85cb2
aliashrafabbasi <aliashrafabbasi3@gmail.com>
2026-09-06 02:14:13 +0500

    feat: boot minimal kernel with Limine

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,18 +1,30 @@
 #include <stdint.h>
+#include "../limine.h"
 
-void kernel_main(void)
+__attribute__((used, section(".limine_requests")))
+static volatile uint64_t base_revision[] =
+    LIMINE_BASE_REVISION(3);
+
+__attribute__((used, section(".limine_requests_start")))
+static volatile uint64_t requests_start_marker[] =
+    LIMINE_REQUESTS_START_MARKER;
+
+__attribute__((used, section(".limine_requests_end")))
+static volatile uint64_t requests_end_marker[] =
+    LIMINE_REQUESTS_END_MARKER;
+
+static void hcf(void)
 {
-    volatile uint16_t *video = (volatile uint16_t *)0xB8000;
-
-    const char *message = "BATOS OS - Kernel Booted!";
-
-    for (uint64_t i = 0; message[i] != '\0'; i++)
-    {
-        video[i] = ((uint16_t)0x07 << 8) | message[i];
-    }
-
     for (;;)
     {
         __asm__ volatile ("hlt");
     }
+}
+
+void kernel_main(void)
+{
+    if (!LIMINE_BASE_REVISION_SUPPORTED(base_revision))
+        hcf();
+
+    hcf();
 }
