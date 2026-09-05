7f2cd9a40a14823aea2126ab175ff27013fb6db4
aliashrafabbasi <aliashrafabbasi3@gmail.com>
2026-09-06 01:09:02 +0500

    feat: add initial 64-bit kernel

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,16 +1,17 @@
+#include <stdint.h>
+
 void kernel_main(void)
 {
-    volatile char *video = (volatile char *)0xB8000;
+    volatile uint16_t *video = (volatile uint16_t *)0xB8000;
 
     const char *message = "BATOS OS - Kernel Booted!";
 
-    for (int i = 0; message[i] != '\0'; i++)
+    for (uint64_t i = 0; message[i] != '\0'; i++)
     {
-        video[i * 2] = message[i];
-        video[i * 2 + 1] = 0x07;
+        video[i] = ((uint16_t)0x07 << 8) | message[i];
     }
 
-    while (1)
+    for (;;)
     {
         __asm__ volatile ("hlt");
     }
